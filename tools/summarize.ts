c3285d18fb93ad56461a418356a004ee86adce7e
Paul Kinlan <paul.kinlan@gmail.com>
2023-10-18 17:39:33 +0000

    Mostly end-to-end

--- tools/summarize.ts
+++ tools/summarize.ts
@@ -1,10 +1,12 @@
 import { Board } from "@google-labs/breadboard";
+
+import '@anthropic-ai/sdk/shims/web'; // this is needed because Anthopic SDK makes some false assumptions about the environment
 import { Claude } from "@paulkinlan/claude-breadboard-kit";
 
 import path from "path";
 
 const board = await Board.load(
-  path.join(process.cwd(), "tools", "graphs", "test.json"),
+  path.join(process.cwd(), "tools", "graphs", "summarize.json"),
   {
     "kits": {
       "@paulkinlan/claude-breadboard-kit": Claude
@@ -12,9 +14,11 @@ const board = await Board.load(
   }
 );
 
+console.log("Running board");
+
 const result = await board.runOnce({
-  model: "claude-2",
-  text: "37917597",
+  "model": "claude2",
+  "input-hacker-news": "37917597"
 });
 
-console.log(result.text as string)
+console.log(result)
